7df33d62720c16792ff7dae26a1ad7eb83be8447
Avogardo <avogardo0@gmail.com>
2018-06-01 17:58:26 +0200

    add linter

--- imports/startup/client/App.jsx
+++ imports/startup/client/App.jsx
@@ -1,14 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { render } from 'react-dom';
- 
-const App = () => 
-	<h1>Hello world</h1>
-;
+
+const App = () => <h1>Hello world</h1>;
 
 Meteor.startup(() => {
-    render(
-        <App />,
-        document.querySelector('#app'),
-    );
+  render(
+    <App />,
+    document.querySelector('#app'),
+  );
 });
