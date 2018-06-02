3ad654bcaef79f98c9fee81bd97b639a197d86c0
Avogardo <avogardo0@gmail.com>
2018-06-02 21:11:15 +0200

    add font, layout css, header

--- imports/components/MainLayout/MainLayout.jsx
+++ imports/components/MainLayout/MainLayout.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import './MainLayout.css';
 
 const MainLayout = () => (
   <div>
-    <h1>Hello world</h1>
+    <header>
+      <img alt="logo" src="https://drive.google.com/uc?id=1U_HEoR8c2kubf6-JsbHEcwo564J5zjlE" />
+      <h1>Alarms blog</h1>
+    </header>
+
     <Button variant="raised" color="secondary">
       Hello World
     </Button>
