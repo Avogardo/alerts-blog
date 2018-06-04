8cef842d935e9c204c19b8d99723ebeaa9983700
Avogardo <jakub.i.wolny@gmail.com>
2018-06-04 19:01:36 +0200

    added snapshot test

--- src/MainLayout.test.jsx
+++ src/MainLayout.test.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainLayout from '../imports/components/MainLayout';
 import TestRenderer from 'react-test-renderer';
 import ShallowRenderer from 'react-test-renderer/shallow';
+import MainLayout from '../imports/components/MainLayout';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<MainLayout />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders correctly all elements', () => {
+  const tree = TestRenderer.create(<MainLayout />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
