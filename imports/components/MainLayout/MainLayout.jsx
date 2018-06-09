a4e81491757de46940328313a63adf2674d2d482
Avogardo <jakub.i.wolny@gmail.com>
2018-06-09 22:19:16 +0200

    pluged sidebar component into main loayout

--- imports/components/MainLayout/MainLayout.jsx
+++ imports/components/MainLayout/MainLayout.jsx
@@ -6,7 +6,9 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Search from '@material-ui/icons/Search';
 import NewsContainer from '../NewsContainer';
+
 import './MainLayout.css';
+import Sidebar from '../Sidebar';
 
 const style = {
   appBar: {
@@ -23,23 +25,33 @@ const style = {
   },
 };
 
-const MainLayout = () => (
-  <div>
-    <AppBar style={style.appBar} position="static">
-      <Toolbar style={style.toolbar}>
-        <div>
-          <IconButton color="inherit">
-            <svg viewBox="0 0 24 24">
-              <path
-                d="M17,2V2H17V6H15C14.31,6 14,6.81 14,7.5V10H14L17,10V14H14V22H10V14H7V10H10V6A4,4 0 0,1 14,2H17Z"
-              />
-            </svg>
-          </IconButton>
+class MainLayout extends React.Component {
+  isSidebarOpen = false;
 
-          <IconButton color="inherit">
-            <svg viewBox="0 0 24 24">
-              <path
-                d="M10,15L15.19,12L10,9V15M21.56,7.17C21.69,7.64 21.78,8.27 21.84,9.07C21.91,
+  toggleSidebar = () => () => {
+    this.setState({
+      isSidebarOpen: !this.state.isSidebarOpen,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <AppBar style={style.appBar} position="static">
+          <Toolbar style={style.toolbar}>
+            <div>
+              <IconButton color="inherit">
+                <svg viewBox="0 0 24 24">
+                  <path
+                    d="M17,2V2H17V6H15C14.31,6 14,6.81 14,7.5V10H14L17,10V14H14V22H10V14H7V10H10V6A4,4 0 0,1 14,2H17Z"
+                  />
+                </svg>
+              </IconButton>
+
+              <IconButton color="inherit">
+                <svg viewBox="0 0 24 24">
+                  <path
+                    d="M10,15L15.19,12L10,9V15M21.56,7.17C21.69,7.64 21.78,8.27 21.84,9.07C21.91,
                   9.87 21.94,10.56 21.94,11.16L22,12C22,14.19 21.84,15.8 21.56,16.83C21.31,
                   17.73 20.73,18.31 19.83,18.56C19.36,18.69 18.5,18.78 17.18,18.84C15.88,
                   18.91 14.69,18.94 13.59,18.94L12,19C7.81,19 5.2,18.84 4.17,18.56C3.27,
@@ -47,45 +59,49 @@ const MainLayout = () => (
                   13.44 2.06,12.84L2,12C2,9.81 2.16,8.2 2.44,7.17C2.69,6.27 3.27,5.69 4.17,
                   5.44C4.64,5.31 5.5,5.22 6.82,5.16C8.12,5.09 9.31,5.06 10.41,5.06L12,5C16.19,
                   5 18.8,5.16 19.83,5.44C20.73,5.69 21.31,6.27 21.56,7.17Z"
-              />
-            </svg>
-          </IconButton>
-        </div>
+                  />
+                </svg>
+              </IconButton>
+            </div>
 
-        <IconButton color="inherit">
-          <svg viewBox="0 0 24 24">
-            <path
-              d="M4,4H20A2,2 0 0,1 22,6V18A2,2 0 0,1 20,20H4C2.89,20 2,19.1 2,18V6C2,4.89 2.89,
+            <IconButton color="inherit">
+              <svg viewBox="0 0 24 24">
+                <path
+                  d="M4,4H20A2,2 0 0,1 22,6V18A2,2 0 0,1 20,20H4C2.89,20 2,19.1 2,18V6C2,4.89 2.89,
                 4 4,4M12,11L20,6H4L12,11M4,18H20V8.37L12,13.36L4,8.37V18Z"
-            />
-          </svg>
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+                />
+              </svg>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
 
-    <div className="logo-container">
-      <img alt="logo" src="https://drive.google.com/uc?id=1U_HEoR8c2kubf6-JsbHEcwo564J5zjlE" />
-      <h1>Alarms blog</h1>
-    </div>
-
-    <AppBar style={style.secondaryAppBar} position="sticky">
-      <Toolbar style={style.toolbar}>
-        <div className="navigation-wrapper">
-          <IconButton color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="title" color="inherit">
-            Menu
-          </Typography>
+        <div className="logo-container">
+          <img alt="logo" src="https://drive.google.com/uc?id=1U_HEoR8c2kubf6-JsbHEcwo564J5zjlE" />
+          <h1>Alarms blog</h1>
         </div>
-        <IconButton color="inherit">
-          <Search />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
 
-    <NewsContainer />
-  </div>
-);
+        <AppBar style={style.secondaryAppBar} position="sticky">
+          <Toolbar style={style.toolbar}>
+            <div className="navigation-wrapper">
+              <IconButton onClick={() => this.toggleSidebar(false)} color="inherit" aria-label="Menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="title" color="inherit">
+                Menu
+              </Typography>
+            </div>
+            <IconButton color="inherit">
+              <Search />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+
+        <Sidebar isSidebarOpen={false} toggleSidebar={() => this.toggleSidebar()} />
+
+        <NewsContainer />
+      </div>
+    );
+  }
+}
 
 export default MainLayout;
--- imports/components/Sidebar/Sidebar.jsx
+++ imports/components/Sidebar/Sidebar.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
-import { mailFolderListItems, otherMailFolderListItems } from './tileData';
+
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import StarIcon from '@material-ui/icons/Star';
+import SendIcon from '@material-ui/icons/Send';
+import MailIcon from '@material-ui/icons/Mail';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ReportIcon from '@material-ui/icons/Report';
 
 const styles = {
   list: {
@@ -16,6 +25,58 @@ const styles = {
 const Sidebar = (props) => {
   const { classes, isSidebarOpen, toggleSidebar } = props;
 
+  const mailFolderListItems = (
+    <div>
+      <ListItem button>
+        <ListItemIcon>
+          <InboxIcon />
+        </ListItemIcon>
+        <ListItemText primary="Inbox" />
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <StarIcon />
+        </ListItemIcon>
+        <ListItemText primary="Starred" />
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <SendIcon />
+        </ListItemIcon>
+        <ListItemText primary="Send mail" />
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <DraftsIcon />
+        </ListItemIcon>
+        <ListItemText primary="Drafts" />
+      </ListItem>
+    </div>
+  );
+
+  const otherMailFolderListItems = (
+    <div>
+      <ListItem button>
+        <ListItemIcon>
+          <MailIcon />
+        </ListItemIcon>
+        <ListItemText primary="All mail" />
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <DeleteIcon />
+        </ListItemIcon>
+        <ListItemText primary="Trash" />
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <ReportIcon />
+        </ListItemIcon>
+        <ListItemText primary="Spam" />
+      </ListItem>
+    </div>
+  );
+
   const sideList = (
     <div className={classes.list}>
       <List>{mailFolderListItems}</List>
@@ -25,33 +86,27 @@ const Sidebar = (props) => {
   );
 
   return (
-    <div>
-      <Button onClick={toggleSidebar(true)}>Open Left</Button>
-
-      <SwipeableDrawer
-        open={isSidebarOpen}
-        onClose={toggleSidebar(false)}
-        onOpen={toggleSidebar(true)}
+    <SwipeableDrawer
+      open={isSidebarOpen}
+      onClose={toggleSidebar(false)}
+      onOpen={toggleSidebar(true)}
+    >
+      <div
+        tabIndex={0}
+        role="button"
+        onClick={toggleSidebar(false)}
+        onKeyDown={toggleSidebar(false)}
       >
-        <div
-          tabIndex={0}
-          role="button"
-          onClick={toggleSidebar(false)}
-          onKeyDown={toggleSidebar(false)}
-        >
-          {sideList}
-        </div>
-      </SwipeableDrawer>
-    </div>
+        {sideList}
+      </div>
+    </SwipeableDrawer>
   );
 };
 
 
 Sidebar.propTypes = {
   classes: PropTypes.shape({
-    list: PropTypes.shape({
-      width: PropTypes.number.isRequired,
-    }).isRequired,
+    list: PropTypes.string.isRequired,
   }).isRequired,
   isSidebarOpen: PropTypes.bool.isRequired,
   toggleSidebar: PropTypes.func.isRequired,
