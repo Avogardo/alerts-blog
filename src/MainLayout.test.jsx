2c488181b114efad54158146f2494b32cb367478
Avogardo <jakub.i.wolny@gmail.com>
2018-06-18 21:14:28 +0200

    removed snapshoot test

--- src/MainLayout.test.jsx
+++ src/MainLayout.test.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestRenderer from 'react-test-renderer';
-import ShallowRenderer from 'react-test-renderer/shallow';
 import MainLayout from '../imports/components/MainLayout';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<MainLayout />, div);
   ReactDOM.unmountComponentAtNode(div);
-});
-
-it('renders correctly all elements', () => {
-  const tree = TestRenderer.create(<MainLayout />).toJSON();
-  expect(tree).toMatchSnapshot();
 });
