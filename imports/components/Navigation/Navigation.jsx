157a4937ec137a7a598e0e386941334a74ca5369
Avogardo <jakub.i.wolny@gmail.com>
2018-06-19 18:33:53 +0200

    destructuring styles and props in nav component

--- imports/components/Navigation/Navigation.jsx
+++ imports/components/Navigation/Navigation.jsx
@@ -27,48 +27,52 @@ const style = {
   },
 };
 
-const Navigation = props => (
-  <nav>
-    <AppBar style={style.appBar} position="static">
-      <Toolbar style={style.toolbar}>
-        <div>
-          <IconButton color="inherit">
-            <FacebookIcon />
-          </IconButton>
+const Navigation = ({ toggleSidebar }) => {
+  const { appBar, toolbar, secondaryAppBar } = style;
+
+  return (
+    <nav>
+      <AppBar style={appBar} position="static">
+        <Toolbar style={toolbar}>
+          <div>
+            <IconButton color="inherit">
+              <FacebookIcon />
+            </IconButton>
+
+            <IconButton color="inherit">
+              <YoutubeIcon />
+            </IconButton>
+          </div>
 
           <IconButton color="inherit">
-            <YoutubeIcon />
+            <EmailIcon />
           </IconButton>
-        </div>
+        </Toolbar>
+      </AppBar>
 
-        <IconButton color="inherit">
-          <EmailIcon />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+      <div className="logo-container">
+        <img alt="logo" src="https://drive.google.com/uc?id=1U_HEoR8c2kubf6-JsbHEcwo564J5zjlE" />
+        <h1>Alarms blog</h1>
+      </div>
 
-    <div className="logo-container">
-      <img alt="logo" src="https://drive.google.com/uc?id=1U_HEoR8c2kubf6-JsbHEcwo564J5zjlE" />
-      <h1>Alarms blog</h1>
-    </div>
-
-    <AppBar style={style.secondaryAppBar} position="sticky">
-      <Toolbar style={style.toolbar}>
-        <div className="navigation-wrapper">
-          <IconButton onClick={() => props.toggleSidebar()} color="inherit" aria-label="Menu">
-            <MenuIcon />
+      <AppBar style={secondaryAppBar} position="sticky">
+        <Toolbar style={toolbar}>
+          <div className="navigation-wrapper">
+            <IconButton onClick={() => toggleSidebar()} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit">
+              Menu
+            </Typography>
+          </div>
+          <IconButton color="inherit">
+            <Search />
           </IconButton>
-          <Typography variant="title" color="inherit">
-            Menu
-          </Typography>
-        </div>
-        <IconButton color="inherit">
-          <Search />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
-  </nav>
-);
+        </Toolbar>
+      </AppBar>
+    </nav>
+  );
+};
 
 Navigation.propTypes = {
   toggleSidebar: PropTypes.func.isRequired,
