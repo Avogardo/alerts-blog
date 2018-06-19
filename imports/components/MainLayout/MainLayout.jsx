c70c84a9ec9a619a65c86d094a3547c7131d9e9d
Avogardo <jakub.i.wolny@gmail.com>
2018-06-19 18:26:07 +0200

    created constructor in mainlayout, move one bind to constructor

--- imports/components/MainLayout/MainLayout.jsx
+++ imports/components/MainLayout/MainLayout.jsx
@@ -1,28 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Navigation from '../Navigation';
 import NewsContainer from '../NewsContainer';
 import Sidebar from '../Sidebar';
 
-class MainLayout extends React.Component {
-  state = {
-    isSidebarOpen: false,
-  };
+class MainLayout extends Component {
+  constructor(props) {
+    super(props);
+    this.toggleSidebar = this.toggleSidebar.bind(this);
 
-  toggleSidebar = () => () => {
+    this.state = {
+      isSidebarOpen: false,
+    };
+  }
+
+  toggleSidebar() {
     this.setState({
       isSidebarOpen: !this.state.isSidebarOpen,
     });
-  };
+  }
 
   render() {
     const { isSidebarOpen } = this.state;
 
     return (
       <div>
-        <Navigation toggleSidebar={this.toggleSidebar()} />
+        <Navigation toggleSidebar={() => this.toggleSidebar()} />
 
-        <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={this.toggleSidebar()} />
+        <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={() => this.toggleSidebar()} />
 
         <NewsContainer />
       </div>
