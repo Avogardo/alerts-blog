d4f9acf47d7d01b4e91d374458d5674486c9ea6f
Avogardo <jakub.i.wolny@gmail.com>
2018-06-21 17:06:51 +0200

    added enzyme

--- src/Sidebar.test.jsx
+++ src/Sidebar.test.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import TestRenderer from 'react-test-renderer';
 import Sidebar from '../imports/components/Sidebar';
 
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+import { mount } from 'enzyme';
+
+
 let props = {};
 const div = document.createElement('div');
 const styles = {
@@ -27,3 +33,8 @@ it('renders without crashing', () => {
     div,
   );
 });
+
+it('renders without crashing', () => {
+  const wrapper = mount(<Sidebar {...props} />);
+});
+
