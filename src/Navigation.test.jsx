7be56090d233a72a1769837b5f48d5884161fd71
Avogardo <avogardo0@gmail.com>
2018-06-21 12:48:54 +0200

    added navigation test file

---
+++ src/Navigation.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestRenderer from 'react-test-renderer';
+import Navigation from '../imports/components/Navigation';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Navigation toggleSidebar={() => {}} />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
