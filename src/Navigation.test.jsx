2113a6ab72afb1c0a92411aaeb9eb4d9ede778ed
Avogardo <avogardo0@gmail.com>
2018-06-24 15:17:33 +0200

    described nagication tests

--- src/Navigation.test.jsx
+++ src/Navigation.test.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import TestRenderer from 'react-test-renderer';
 import Navigation from '../imports/components/Navigation';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Navigation toggleSidebar={() => {}} />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("Navigation", () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Navigation toggleSidebar={() => {}} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
