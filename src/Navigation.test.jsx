2037678f594b6546aa7289362c653b58e8a21b0b
Avogardo <avogardo0@gmail.com>
2018-06-25 13:46:10 +0200

    added always renders both app bars test

--- src/Navigation.test.jsx
+++ src/Navigation.test.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestRenderer from 'react-test-renderer';
+
+import AppBar from '@material-ui/core/AppBar';
 import Navigation from '../imports/components/Navigation';
 
 describe("Navigation", () => {
@@ -23,5 +25,12 @@ describe("Navigation", () => {
     const testInstance = testRenderer.root;
 
     expect(testInstance.findAllByType('nav').length).toBe(1);
+  });
+
+  it('always renders both app bars', () => {
+    const testRenderer = TestRenderer.create(<Navigation {...props} />);
+    const testInstance = testRenderer.root;
+
+    expect(testInstance.findAllByType(AppBar).length).toBe(2);
   });
 });
