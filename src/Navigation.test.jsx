b9b7a28f3ae6768e65a86295e3784165df8b8068
Avogardo <avogardo0@gmail.com>
2018-06-25 13:44:39 +0200

    added always renders a nav bar test

--- src/Navigation.test.jsx
+++ src/Navigation.test.jsx
@@ -4,9 +4,24 @@ import TestRenderer from 'react-test-renderer';
 import Navigation from '../imports/components/Navigation';
 
 describe("Navigation", () => {
+  let props;
+
+  beforeEach(() => {
+    props = {
+      toggleSidebar: () => {},
+    };
+  });
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Navigation toggleSidebar={() => {}} />, div);
+    ReactDOM.render(<Navigation {...props} />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('always renders a nav bar', () => {
+    const testRenderer = TestRenderer.create(<Navigation {...props} />);
+    const testInstance = testRenderer.root;
+
+    expect(testInstance.findAllByType('nav').length).toBe(1);
   });
 });
