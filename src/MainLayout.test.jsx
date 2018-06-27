3fb3be30cb9014f0803e2da2f3f21bd0bef9c352
Avogardo <avogardo0@gmail.com>
2018-06-27 12:05:47 +0200

    added enzyme and always renders a Navigation component test

--- src/MainLayout.test.jsx
+++ src/MainLayout.test.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TestRenderer from 'react-test-renderer';
+
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
 
 import MainLayout from '../imports/components/MainLayout';
+import Navigation from '../imports/components/Navigation';
+import Sidebar from '../imports/components/Sidebar';
+import NewsContainer from '../imports/components/NewsContainer';
 
 describe("Main layout", () => {
+  let mountedComponent;
+  const MainLayoutComponent = () => {
+    if (!mountedComponent) {
+      mountedComponent = mount(
+        <MainLayout />
+      );
+    }
+    return mountedComponent;
+  };
+
+  beforeEach(() => {
+    mountedComponent = undefined;
+  });
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<MainLayout />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('always renders a Navigation component', () => {
+    expect(MainLayoutComponent().find(Navigation)).toBeDefined();
   });
 });
