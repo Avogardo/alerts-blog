14a69794a304fd6d00c3570a04f00df565ea7a51
Avogardo <avogardo0@gmail.com>
2018-06-27 11:21:42 +0200

    added main layout test description

--- src/MainLayout.test.jsx
+++ src/MainLayout.test.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestRenderer from 'react-test-renderer';
+
 import MainLayout from '../imports/components/MainLayout';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MainLayout />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("Main layout", () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<MainLayout />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
