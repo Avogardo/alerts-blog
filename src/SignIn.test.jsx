4031d295476d3d0211aa9dda95200f67778958c1
Avogardo <avogardo0@gmail.com>
2018-06-28 10:35:18 +0200

    created sign in component test file

---
+++ src/SignIn.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestRenderer from 'react-test-renderer';
+
+import SignIn from '../imports/components/SignIn';
+
+describe("SignIn", () => {
+  let testRenderer, testInstance;
+
+  beforeEach(() => {
+    testRenderer = TestRenderer.create(<SignIn />);
+    testInstance = testRenderer.root;
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<SignIn />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
