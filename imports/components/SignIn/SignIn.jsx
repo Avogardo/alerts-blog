04454f64b6afaeb8d52a0be39ff0d7d0108f8fec
Avogardo <avogardo0@gmail.com>
2018-06-28 10:48:30 +0200

    added google plus button

--- imports/components/SignIn/SignIn.jsx
+++ imports/components/SignIn/SignIn.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+
+import Button from '@material-ui/core/Button';
+import Delete from '@material-ui/icons/Delete';
+
 import './SignIn.css';
 
 const SignIn = () => (
   <section>
     <h2>Create account with Google Plus!</h2>
+
+    <Button variant="raised" color="secondary">
+      Google +
+      <Delete />
+    </Button>
   </section>
 );
 
