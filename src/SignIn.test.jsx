8a1fb2fc9c61cb3cce888f5017520c0c872df068
Avogardo <avogardo0@gmail.com>
2018-07-01 10:52:21 +0200

    add props to component

--- src/SignIn.test.jsx
+++ src/SignIn.test.jsx
@@ -3,19 +3,29 @@ import ReactDOM from 'react-dom';
 import TestRenderer from 'react-test-renderer';
 
 import Button from '@material-ui/core/Button';
-import SignIn from '../imports/components/SignIn';
+import SignIn from '../imports/components/SignIn/SignIn.jsx';
 
 describe("SignIn", () => {
-  let testRenderer, testInstance;
+  let props, testRenderer, testInstance;
 
   beforeEach(() => {
-    testRenderer = TestRenderer.create(<SignIn />);
+    props = {
+      classes: {
+        button: '',
+        rightIcon: '',
+        signIn: '',
+      },
+      errorMessage: '',
+      onGoogleLogin: () => {},
+    }
+
+    testRenderer = TestRenderer.create(<SignIn {...props} />);
     testInstance = testRenderer.root;
   });
 
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<SignIn />, div);
+    ReactDOM.render(<SignIn {...props} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
