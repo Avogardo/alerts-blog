3c7c0a16238fbb4cab4fadaa88b12501dded625a
Avogardo <jakub.i.wolny@gmail.com>
2018-07-03 21:31:39 +0200

    Revert "try to configure jest"
    This reverts commit 5fd005944dabbdbc7accdbec5ca903e97c135bd7.

--- meteorMocks/index.js
+++
@@ -1,9 +0,0 @@
-export const Meteor = {
-  users: {
-    findOne: jest.fn().mockImplementation(() => usersQueryResult),
-    find: jest.fn().mockImplementation(() => ({
-      fetch: jest.fn().mockReturnValue(usersQueryResult),
-      count: jest.fn(),
-    })),
-  },
-};
