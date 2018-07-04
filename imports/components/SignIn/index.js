6350c00271ed524317581cf67938a44aa50bbf07
Avogardo <avogardo0@gmail.com>
2018-07-04 13:42:16 +0200

    if user is logged in he can not go to sign in page

--- imports/components/SignIn/index.js
+++ imports/components/SignIn/index.js
@@ -1,8 +1,16 @@
 import { Meteor } from 'meteor/meteor';
 import { compose } from 'react-komposer';
+import { isLoggedIn } from '/imports/api/users';
 import SignIn from './SignIn.jsx';
+import { actions as newsActions } from '../../api/news';
 
 const composer = (props, onData) => {
+  const isLoggedInUser = isLoggedIn();
+
+  if (isLoggedInUser) {
+    newsActions.goToNewsContainer(props.history);
+  }
+
   const onGoogleLogin = () => Meteor.loginWithGoogle(
     { requestPermissions: ['email'] },
     (error) => {
