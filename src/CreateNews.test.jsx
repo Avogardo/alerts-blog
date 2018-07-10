fa87d4c4b2199ce9fddf58562d0d2af9795c63d3
Avogardo <avogardo0@gmail.com>
2018-07-10 12:17:53 +0200

    add always renders a form test

--- src/CreateNews.test.jsx
+++ src/CreateNews.test.jsx
@@ -4,14 +4,20 @@ import TestRenderer from 'react-test-renderer';
 import CreateNews from '../imports/components/CreateNews/CreateNews.jsx';
 
 describe("Create news", () => {
+  let testRenderer, testInstance;
+
+  beforeEach(() => {
+    testRenderer = TestRenderer.create(<CreateNews />);
+    testInstance = testRenderer.root;
+  });
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
-
-    ReactDOM.render(
-      <CreateNews />,
-      div,
-    );
-
+    ReactDOM.render(<CreateNews />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('always renders a form', () => {
+    expect(testInstance.findAllByType('form')).toHaveLength(1)
   });
 });
