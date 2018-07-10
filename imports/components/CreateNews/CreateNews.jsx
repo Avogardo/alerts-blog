ad0f82dd4a39c724358fc9ee6ceb2ec94a766411
Avogardo <avogardo0@gmail.com>
2018-07-10 12:00:52 +0200

    add all necessary elements to render method without styling

--- imports/components/CreateNews/CreateNews.jsx
+++ imports/components/CreateNews/CreateNews.jsx
@@ -1,5 +1,57 @@
 import React from 'react';
 
-const CreateNews = () => <h2>Create news</h2>;
+import {
+  Button,
+  CardHeader,
+  TextField,
+  CardActions,
+  Chip,
+} from '@material-ui/core';
+
+const CreateNews = () => (
+  <form>
+    <CardHeader title="Create news" />
+    <CardActions>
+      <TextField
+        label="Post title"
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        label="Post content"
+        fullWidth
+        multiline
+        margin="normal"
+        rowsMax="15"
+      />
+
+      <input
+        accept="image/*"
+        id="raised-button-file"
+        multiple
+        type="file"
+      />
+      <label htmlFor="raised-button-file">
+        <Button variant="raised" component="span">
+          Upload images
+        </Button>
+      </label>
+
+      <TextField
+        label="Type tags"
+        fullWidth
+        margin="normal"
+      />
+      <Chip label="data.label" />
+
+      <Button variant="flat" color="secondary">
+        Clear
+      </Button>
+      <Button variant="raised" color="primary">
+        Create
+      </Button>
+    </CardActions>
+  </form>
+);
 
 export default CreateNews;
