7fbaf016c2b256fab362c5b0dd2e12ed3a235a51
Avogardo <avogardo0@gmail.com>
2018-07-13 22:43:56 +0200

    add recentNewsWithLimit publication

--- imports/api/news/server/publications.js
+++ imports/api/news/server/publications.js
@@ -4,3 +4,11 @@ import News from './../News.js';
 Meteor.publish('news', function publishNewsList() {
   return News.find({});
 });
+
+Meteor.publish('recentNewsWithLimit', function publishRecentNewsLimit(limit = 3) {
+  const options = {
+    limit,
+    sort: { createdAt: -1 },
+  };
+  return News.find({}, options);
+});
