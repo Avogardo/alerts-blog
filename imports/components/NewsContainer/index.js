88b8004e06ca2968b9d28db676646098bf955539
Avogardo <avogardo0@gmail.com>
2018-07-13 22:44:09 +0200

    subscribe recentNewsWithLimit

--- imports/components/NewsContainer/index.js
+++ imports/components/NewsContainer/index.js
@@ -1,12 +1,37 @@
+import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
 import { compose } from 'react-komposer';
+import { News as NewsCollection } from '/imports/api/news';
 import NewsContainer from './NewsContainer.jsx';
 
-const composer = (props, onData) => {
-  const newsArray = [1];
+const getTrackerLoader = composer =>
+  (props, onData, env) => {
+    let trackerCleanup = null;
+    const handler = Tracker.nonreactive(() =>
+      Tracker.autorun(() => {
+        // assign the custom clean-up function.
+        trackerCleanup = composer(props, onData, env);
+      }));
 
-  onData(null, {
-    newsArray,
-  });
+    return () => {
+      if (typeof trackerCleanup === 'function') trackerCleanup();
+      return handler.stop();
+    };
+  };
+
+const composer = (props, onData) => {
+  const topNewsHandler = Meteor.subscribe('recentNewsWithLimit');
+
+  if (topNewsHandler) {
+    const topNews = NewsCollection.find().fetch();
+    console.log(topNews);
+
+    onData(null, {
+      topNews,
+    });
+  }
+
+  onData(null, {});
 };
 
-export default compose(composer)(NewsContainer);
+export default compose(getTrackerLoader(composer))(NewsContainer);
