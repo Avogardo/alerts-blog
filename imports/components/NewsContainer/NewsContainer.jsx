5f238f21ac393da1e75fc4ed03be18507338f460
Avogardo <avogardo0@gmail.com>
2018-07-17 11:11:10 +0200

    move composers to one file, to avoid redundant code

--- imports/components/NewsContainer/NewsContainer.jsx
+++ imports/components/NewsContainer/NewsContainer.jsx
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import EnterNewsGrid from './EnterNewsGrid';
 import BasicNewsGrid from './BasicNewsGrid';
 
-const NewsContainer = ({ enterContainer }) => (
-  enterContainer ? <EnterNewsGrid /> : <BasicNewsGrid />
-);
+class NewsContainer extends Component {
+  render() {
+    const {
+      enterContainer,
+      topNews,
+      authors,
+    } = this.props;
+
+    return (
+      enterContainer ?
+        <EnterNewsGrid
+          topNews={topNews}
+          authors={authors}
+        />
+        :
+        <BasicNewsGrid
+          topNews={topNews}
+          authors={authors}
+        />
+    );
+  }
+}
 
 NewsContainer.defaultProps = {
   enterContainer: false,
+  topNews: [],
+  authors: [],
 };
 
 NewsContainer.propTypes = {
   enterContainer: PropTypes.bool,
+  topNews: PropTypes.arrayOf(PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    authorId: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    createdAt: PropTypes.instanceOf(Date).isRequired,
+    tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+    enterImage: PropTypes.shape({
+      data: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        image: PropTypes.instanceOf(Uint8Array).isRequired,
+      }),
+    }).isRequired,
+  }).isRequired),
+  authors: PropTypes.arrayOf(PropTypes.string.isRequired),
 };
 
 export default NewsContainer;
--- imports/components/NewsContainer/index.js
+++ imports/components/NewsContainer/index.js
@@ -1,3 +1,51 @@
+import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
+import { compose } from 'react-komposer';
+import { News as NewsCollection } from '/imports/api/news';
 import NewsContainer from './NewsContainer.jsx';
 
-export default NewsContainer;
+const getTrackerLoader = composer =>
+  (props, onData, env) => {
+    let trackerCleanup = null;
+    const handler = Tracker.nonreactive(() =>
+      Tracker.autorun(() => {
+        // assign the custom clean-up function.
+        trackerCleanup = composer(props, onData, env);
+      }));
+
+    return () => {
+      if (typeof trackerCleanup === 'function') trackerCleanup();
+      return handler.stop();
+    };
+  };
+
+const composer = (props, onData) => {
+  const topNewsHandler = Meteor.subscribe('recentNewsWithLimit', 4);
+  const userListHandler = Meteor.subscribe('userList');
+
+  if (topNewsHandler.ready()) {
+    const options = {
+      limit: props.enterContainer ? 3 : 4,
+    };
+    const topNews = NewsCollection.find({}, options).fetch();
+
+    onData(null, {
+      topNews,
+    });
+
+    if (userListHandler.ready()) {
+      const users = Meteor.users.find({}).fetch();
+      const authors = topNews.map(news =>
+        users.find(user => user._id === news.authorId).profile.name);
+
+      onData(null, {
+        topNews,
+        authors,
+      });
+    }
+  } else {
+    onData(null, {});
+  }
+};
+
+export default compose(getTrackerLoader(composer))(NewsContainer);
