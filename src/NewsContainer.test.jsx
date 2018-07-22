de83e0df3038dce45fe0205fa3421eed069d103b
Avogardo <avogardo0@gmail.com>
2018-07-22 14:45:31 +0200

    describe news container tests

--- src/NewsContainer.test.jsx
+++ src/NewsContainer.test.jsx
@@ -3,11 +3,46 @@ import ReactDOM from 'react-dom';
 import TestRenderer from 'react-test-renderer';
 import NewsContainer from '../imports/components/NewsContainer/NewsContainer.jsx';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <NewsContainer />,
-    div,
-  );
-  ReactDOM.unmountComponentAtNode(div);
+describe('NewsContainer', () => {
+  let props;
+  let testRenderer;
+  let testInstance;
+
+  beforeEach(() => {
+    props = {
+      unit8ArrayToUrl: () => {},
+      classes: {
+        mainNewsCard: '',
+        newsCard: '',
+      },
+    };
+
+    testRenderer = TestRenderer.create(<NewsContainer {...props} />);
+    testInstance = testRenderer.root;
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<NewsContainer {...props} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  describe('Enter container', () => {
+    beforeEach(() => {
+      props = {
+        unit8ArrayToUrl: () => {},
+        enterContainer: true,
+        classes: {
+          mainNewsCard: '',
+          newsCard: '',
+        },
+      };
+    });
+
+    it('renders without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<NewsContainer {...props} />, div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+  });
 });
