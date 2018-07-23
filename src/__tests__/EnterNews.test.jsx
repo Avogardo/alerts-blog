e4b93dcbdb765aa72bfa1173f03390669a42645e
Avogardo <avogardo0@gmail.com>
2018-07-23 11:05:24 +0200

    describe no news section and add renders without crashing test

--- src/__tests__/EnterNews.test.jsx
+++ src/__tests__/EnterNews.test.jsx
@@ -21,9 +21,39 @@ describe('EnterNews', () => {
     testInstance = testRenderer.root;
   });
 
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<EnterNews {...props} />, div);
-    ReactDOM.unmountComponentAtNode(div);
+  describe('There are news', () => {
+    beforeEach(() => {
+      props = {
+        unit8ArrayToUrl: () => {},
+        topNews: [{
+          _id: '',
+          authorId: '',
+          title: '',
+          content: '',
+          createdAt: new Date(),
+          tags: [],
+          enterImage: {
+            data: {
+              name: '',
+              image: new Uint8Array(),
+            },
+          },
+        }],
+        authors: [''],
+        classes: {
+          gridListTileBar: '',
+          breakingNews: '',
+        },
+      };
+
+      testRenderer = TestRenderer.create(<EnterNews {...props} />);
+      testInstance = testRenderer.root;
+    });
+
+    it('renders without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<EnterNews {...props} />, div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
   });
 });
