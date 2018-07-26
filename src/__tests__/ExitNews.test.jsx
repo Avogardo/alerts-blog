dff5b4322b1a2950082f61eb66f3bc8bce2f64e1
Avogardo <jakub.i.wolny@gmail.com>
2018-07-26 18:39:42 +0200

    add exit news test file

---
+++ src/__tests__/ExitNews.test.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestRenderer from 'react-test-renderer';
+import ExitNews from '../../imports/components/NewsContainer/ExitNews';
+
+describe('ExitNews', () => {
+  let props;
+  let testRenderer;
+  let testInstance;
+
+  beforeEach(() => {
+    props = {
+      unit8ArrayToUrl: () => {},
+      topNews: [{
+        _id: '',
+        authorId: '',
+        title: 'title',
+        content: '',
+        createdAt: new Date(),
+        tags: [],
+        enterImage: {
+          data: {
+            name: '',
+            image: new Uint8Array(),
+          },
+        },
+      }],
+      classes: {
+        asideNewsCard: '',
+        asideNewsHeaderCard: '',
+      },
+    };
+
+    testRenderer = TestRenderer.create(<ExitNews {...props} />);
+    testInstance = testRenderer.root;
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<ExitNews {...props} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
