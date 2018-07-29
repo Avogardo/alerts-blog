b674f9001852eaa7c6690e0f518cf6ce01c9963f
Avogardo <avogardo0@gmail.com>
2018-07-29 17:57:22 +0200

    style add comment component

--- imports/components/Comments/AddComment/AddComment.jsx
+++ imports/components/Comments/AddComment/AddComment.jsx
@@ -1,36 +1,105 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {
   Card,
   CardHeader,
   CardActions,
-  TextField,
   Button,
+  withStyles,
+  Input,
 } from '@material-ui/core';
+import './AddComment.css';
+
+const styles = {
+  addCommentCard: {
+    backgroundColor: '#ecf0f1',
+    boxShadow: 'unset',
+    padding: '50px 20px',
+    marginTop: 50,
+  },
+  addCommentHeader: {
+    padding: 0,
+    marginBottom: 50,
+  },
+  actionCard: {
+    flexDirection: 'column',
+    padding: 0,
+  },
+  input: {
+    backgroundColor: '#ffffff',
+    padding: '6px 12px',
+    fontSize: 13,
+    color: '#777777',
+    boxSizing: 'border-box',
+    marginBottom: '16px',
+    marginLeft: 0,
+    marginRight: 0,
+    '&:focus': {
+      border: '1px solid #ced4da',
+    },
+  },
+  inputMultiline: {
+    backgroundColor: '#ffffff',
+    padding: 12,
+    fontSize: 13,
+    color: '#777777',
+    boxSizing: 'border-box',
+    marginBottom: '16px',
+    marginLeft: 0,
+    marginRight: 0,
+    '&:focus': {
+      border: '1px solid #ced4da',
+    },
+  },
+  button: {
+    backgroundColor: '#f6214b',
+  },
+};
 
 class AddComment extends Component {
   render() {
-    return (
-      <Card>
-        <CardHeader title="Post Comment" />
+    const {
+      addCommentCard,
+      addCommentHeader,
+      actionCard,
+      input,
+      inputMultiline,
+      button,
+    } = this.props.classes;
 
-        <CardActions>
-          <TextField
-            label="Enter Name"
+    return (
+      <Card className={addCommentCard}>
+        <CardHeader
+          className={addCommentHeader}
+          title={<h4 className="add-comment-header">Post Comment</h4>}
+        />
+
+        <CardActions className={actionCard}>
+          <Input
+            className={input}
+            placeholder="Enter Name"
+            disableUnderline
             fullWidth
-            margin="normal"
+            inputProps={{
+              'aria-label': 'Description',
+            }}
           />
 
-          <TextField
-            label="Message"
+          <Input
+            className={inputMultiline}
+            placeholder="Message"
+            disableUnderline
             fullWidth
             multiline
-            margin="normal"
-            rowsMax="8"
+            inputProps={{
+              'aria-label': 'Description',
+            }}
           />
 
           <Button
+            className={button}
             variant="raised"
-            color="primary"
+            color="secondary"
           >
             Post Comment
           </Button>
@@ -40,4 +109,17 @@ class AddComment extends Component {
   }
 }
 
-export default AddComment;
+AddComment.propTypes = {
+  classes: PropTypes.shape({
+    addCommentCard: PropTypes.string.isRequired,
+    addCommentHeader: PropTypes.string.isRequired,
+    actionCard: PropTypes.string.isRequired,
+    input: PropTypes.string.isRequired,
+    inputMultiline: PropTypes.string.isRequired,
+    button: PropTypes.string.isRequired,
+  }).isRequired,
+  createComment: PropTypes.func.isRequired,
+};
+
+
+export default withStyles(styles)(AddComment);
