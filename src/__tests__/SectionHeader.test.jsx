3df493ae63525d5153b8e9c066268eafec8b693a
Avogardo <jakub.i.wolny@gmail.com>
2018-07-31 18:05:58 +0200

    add Facebook test section

--- src/__tests__/SectionHeader.test.jsx
+++ src/__tests__/SectionHeader.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TestRenderer from 'react-test-renderer';
 import { CardHeader, IconButton, Card } from '@material-ui/core';
-import { YoutubeIcon } from 'mdi-react';
+import { YoutubeIcon, FacebookIcon } from 'mdi-react';
 import SectionHeader from '../../imports/components/NewsContainer/SectionHeader';
 
 describe('SectionHeader', () => {
@@ -76,6 +76,53 @@ describe('SectionHeader', () => {
 
     it('always render a YoutubeIcon element', () => {
       expect(testInstance.findByType(YoutubeIcon)).toBeDefined();
+    });
+
+    it('always render a social header element', () => {
+      expect(testInstance.findByProps({className: 'social-header'})).toBeDefined();
+    });
+
+    it('always render a social header action element', () => {
+      expect(testInstance.findByProps({className: 'social-header-action'})).toBeDefined();
+    });
+  });
+
+  describe('Facebook', () => {
+    beforeEach(() => {
+      props = {
+        facebook: true,
+        classes: {
+          headerCard: '',
+          secondaryHeaderCard: '',
+          youtubeHeader: '',
+          socialIconButton: '',
+          facebookHeader: '',
+        },
+      };
+
+      testRenderer = TestRenderer.create(<SectionHeader {...props} />);
+      testInstance = testRenderer.root;
+    });
+
+    it('renders without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<SectionHeader {...props} />, div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('always render wrappers inside card header title', () => {
+      expect(testInstance.findByType(CardHeader).findByProps({className: 'social-header-content-wrapper'}))
+        .toBeDefined();
+      expect(testInstance.findByType(CardHeader).findByProps({className: 'social-header-wrapper'}))
+        .toBeDefined();
+    });
+
+    it('always render a IconButton element', () => {
+      expect(testInstance.findByType(IconButton)).toBeDefined();
+    });
+
+    it('always render a FacebookIcon element', () => {
+      expect(testInstance.findByType(FacebookIcon)).toBeDefined();
     });
 
     it('always render a social header element', () => {
