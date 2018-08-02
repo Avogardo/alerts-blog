045d81803a62f9b3401d8311e146ad80723138fd
Avogardo <jakub.i.wolny@gmail.com>
2018-08-02 18:43:06 +0200

    add commentComments publication

--- imports/api/comments/server/publications.js
+++ imports/api/comments/server/publications.js
@@ -5,5 +5,12 @@ import Comments from './../Comments.js';
 Meteor.publish('newsComments', function publishNewsComments(newsId) {
   check(newsId, String);
 
-  return Comments.find({ newsId });
+  return Comments.find({ newsId, parentId: null });
+});
+
+Meteor.publish('commentComments', function publishNewsComments(newsId, parentId) {
+  check(newsId, String);
+  check(parentId, String);
+
+  return Comments.find({ newsId, parentId });
 });
