d1cd762502d62537b1ea6795b2f359bc933e6381
Avogardo <avogardo0@gmail.com>
2018-08-05 17:16:22 +0200

    create component is now reusable, if there is news if props user can edit news

--- imports/components/CreateNews/index.js
+++ imports/components/CreateNews/index.js
@@ -1,14 +1,47 @@
+import { Meteor } from 'meteor/meteor';
 import { compose } from 'react-komposer';
-import { actions as newsActions } from '../../api/news';
+import { Tracker } from 'meteor/tracker';
+import {
+  actions as newsActions,
+  News as NewsCollection,
+} from '../../api/news';
 import CreateNews from './CreateNews.jsx';
+
+const getTrackerLoader = composer =>
+  (props, onData, env) => {
+    let trackerCleanup = null;
+    const handler = Tracker.nonreactive(() =>
+      Tracker.autorun(() => {
+        // assign the custom clean-up function.
+        trackerCleanup = composer(props, onData, env);
+      }));
+
+    return () => {
+      if (typeof trackerCleanup === 'function') trackerCleanup();
+      return handler.stop();
+    };
+  };
 
 const composer = (props, onData) => {
   const newsId = props.match.params.id;
 
-  onData(null, {
-    createNews: newsActions.createNews,
-    ...props,
-  });
+  if (newsId) {
+    const newsHandler = Meteor.subscribe('singleNews', newsId);
+    if (newsHandler.ready()) {
+      const news = NewsCollection.find({ _id: newsId }).fetch()[0];
+
+      onData(null, {
+        news,
+        createNews: newsActions.createNews,
+        ...props,
+      });
+    }
+  } else {
+    onData(null, {
+      createNews: newsActions.createNews,
+      ...props,
+    });
+  }
 };
 
-export default compose(composer)(CreateNews);
+export default compose(getTrackerLoader(composer))(CreateNews);
