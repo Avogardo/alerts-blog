39f19fca5bc9716473ab78d10f28c7bab032960a
Avogardo <jakub.i.wolny@gmail.com>
2018-08-06 16:04:59 +0200

    create addComment test file

---
+++ src/__tests__/AddComment.test.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestRenderer from 'react-test-renderer';
+import {
+  Card,
+  CardHeader,
+  CardActions,
+  Button,
+  withStyles,
+  Input,
+  Snackbar,
+} from '@material-ui/core';
+import AddComment from '../../imports/components/Comments/AddComment/AddComment.jsx';
+
+describe('Add comment', () => {
+  let props;
+  let testRenderer;
+  let testInstance;
+
+  beforeEach(() => {
+    props = {
+      classes: {
+        addCommentCard: '',
+        addCommentHeader: '',
+        actionCard: '',
+        input: '',
+        inputMultiline: '',
+        button: '',
+        addCommentChildrenCard: '',
+        addCommentChildrenHeader: '',
+      },
+      createComment: () => {},
+      isLoggedIn: false,
+    };
+
+    testRenderer = TestRenderer.create(<AddComment {...props} />);
+    testInstance = testRenderer.root;
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<AddComment {...props} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
