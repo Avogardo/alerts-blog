ee913181dcad324ad1f37f5f0fad7a4aa3b33192
Avogardo <avogardo0@gmail.com>
2018-08-10 10:43:50 +0200

    describe Login with Google button test section

---
+++ src/__tests__/LoginWithGoogleButton.test.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestRenderer from 'react-test-renderer';
+import { Button } from '@material-ui/core';
+import LoginWithGoogleButton from '../../imports/components/SignIn/LoginButtons/LoginWithGoogleButton/LoginWithGoogleButton.jsx';
+
+describe('Login with Google button', () => {
+  let props;
+  let testRenderer;
+  let testInstance;
+
+  beforeEach(() => {
+    props = {
+      classes: {
+        button: '',
+        rightIcon: '',
+      },
+      onGoogleLogin: () => {},
+    };
+
+    testRenderer = TestRenderer.create(<LoginWithGoogleButton {...props} />);
+    testInstance = testRenderer.root;
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<LoginWithGoogleButton {...props} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
