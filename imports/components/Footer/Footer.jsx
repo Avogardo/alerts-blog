de912967758cfa5a7db34ce40acacd8bc6a691d7
Avogardo <jakub.i.wolny@gmail.com>
2018-08-22 18:53:25 +0200

    add link to project in footer, style author name

--- imports/components/Footer/Footer.jsx
+++ imports/components/Footer/Footer.jsx
@@ -4,7 +4,15 @@ import './Footer.css';
 const Footer = () => (
   <footer>
     <div className="footer-content">
-      Copyright ©2018 All rights reserved | This page is made by Jakub Wolny
+      Copyright ©2018 All rights reserved | This page is made by <strong className="author-name">Jakub Wolny</strong> |
+      <a
+        className="link-to-github-project"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/Avogardo/alerts-blog"
+      >
+        https://github.com/Avogardo/alerts-blog
+      </a>
     </div>
   </footer>
 );
