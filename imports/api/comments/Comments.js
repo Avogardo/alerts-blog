95e59a592a969a088600e8aa82b6855dc2f2d079
Avogardo <avogardo0@gmail.com>
2018-08-26 16:57:15 +0200

    performance - add newsid index

--- imports/api/comments/Comments.js
+++ imports/api/comments/Comments.js
@@ -1,9 +1,13 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
-
 import { CommentsSchema } from './schema.js';
 
 const Comments = new Mongo.Collection('cmments');
 
 Comments.attachSchema(CommentsSchema);
+
+if (Meteor.isServer) {
+  Comments._ensureIndex({ newsId: -1 });
+}
 
 export default Comments;
