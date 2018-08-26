d0003107f6690a8ca2b97a6103bf11b2e6296be2
Avogardo <avogardo0@gmail.com>
2018-08-26 16:38:56 +0200

    add isBreakingNews index

--- imports/api/news/News.js
+++ imports/api/news/News.js
@@ -1,9 +1,13 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
-
 import { NewsSchema } from './schema.js';
 
 const News = new Mongo.Collection('news');
 
 News.attachSchema(NewsSchema);
+
+if (Meteor.isServer) {
+  News._ensureIndex({ isBreakingNews: -1 });
+}
 
 export default News;
