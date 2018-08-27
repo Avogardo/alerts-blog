b105e60615dae4a56ae291b2bed2e8b84a7f564f
Avogardo <jakub.i.wolny@gmail.com>
2018-08-27 20:25:45 +0200

    add styled components, style footer

--- imports/components/Footer/Footer.jsx
+++ imports/components/Footer/Footer.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 import './Footer.css';
 
+const FooterElement = styled.footer`
+  background-color: black;
+  color: white;
+  padding: 15px;
+  font-size: 14px;
+  font-weight: 300;
+  margin-top: 105px;
+`;
+
 const Footer = () => (
-  <footer>
+  <FooterElement>
     <div className="footer-content">
       Copyright ©2018 All rights reserved | This page is made by <strong className="author-name">Jakub Wolny</strong> |
       <a
@@ -14,7 +24,7 @@ const Footer = () => (
         https://github.com/Avogardo/alerts-blog
       </a>
     </div>
-  </footer>
+  </FooterElement>
 );
 
 export default Footer;
